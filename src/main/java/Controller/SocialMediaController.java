38aa9a69a0478d05b2928bd4b449ef9c09d803e7
Ryan Tavares <RyanTommyTavares@gmail.com>
2024-08-22 21:22:31 +0000

    Remove example-endpoint and handler

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -24,18 +24,9 @@ public class SocialMediaController {
      */
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        app.get("example-endpoint", this::exampleHandler);
         app.post("/register", this::registerHandler);
 
         return app;
-    }
-
-    /**
-     * This is an example handler for an example endpoint.
-     * @param context The Javalin Context object manages information about both the HTTP request and response.
-     */
-    private void exampleHandler(Context context) {
-        context.json("sample text");
     }
 
     private void registerHandler(Context context) throws JsonProcessingException {
